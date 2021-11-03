3499801f726cf8128236aab2c6e7af0381ee8a90
Thuan Bach <ThuanBach@uregina.ca>
2021-11-03 20:26:30 +0700

    Initiate the project

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 3, 2021
+ *      Author: thuanbachvan
+ */
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+	cout << "HELLO WORLD";
+	return 0;
+}
